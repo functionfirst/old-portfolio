5122b309eac8ff60ff97eaa618697d428d790ef0
Alan Jenkins <alan@functionfirst.co.uk>
2015-07-04 15:14:08 +0100

    Added toggle for navigation.

--- public/app/controllers/mainCtrl.js
+++ public/app/controllers/mainCtrl.js
@@ -2,6 +2,10 @@ angular.module('mainCtrl', [])
 
 .controller('MainController', function(){
 	var main = this;
+	main.nav = false;
 
-	
+	main.toggleNav = function(){
+		console.log('toggle nav');
+		main.nav = !main.nav;
+	}
 });
