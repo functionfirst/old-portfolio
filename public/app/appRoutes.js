5120b88108e005e1ae3555e1b8d493b070bcdee2
Alan Jenkins <alan@functionfirst.co.uk>
2015-07-05 22:19:42 +0100

    Replaced old php files with new pages in portfolio controller. Added in prev/next links to transition between portfolio items.

--- public/app/appRoutes.js
+++ public/app/appRoutes.js
@@ -2,23 +2,35 @@ angular.module('appRoutes', ['ngRoute'])
 
 .config(function($routeProvider, $locationProvider) {
 	$routeProvider
-		//homepage
 		.when('/', {
 			templateUrl : '/public/app/views/pages/portfolio/index.html',
 			controller : 'PortfolioController',
 			controllerAs : 'portfolio'
 		})
-		// filter
 		.when('/about', {
 			templateUrl : '/public/app/views/pages/about/index.html',
 			controller : 'AboutController',
 			controllerAs : 'about'
 		})
-		// filter
 		.when('/cv', {
 			templateUrl : '/public/app/views/pages/cv/index.html',
 			controller : 'CVController',
 			controllerAs : 'cv'
+		})
+		.when('/carioca-digital', {
+			templateUrl : '/public/app/views/pages/portfolio/carioca-digital.html',
+			controller : 'PortfolioController',
+			controllerAs : 'cv'
+		})
+		.when('/gulfplates', {
+			templateUrl : '/public/app/views/pages/portfolio/gulfplates.html',
+			controller : 'PortfolioController',
+			controllerAs : 'portfolio'
+		})
+		.when('/buildmate-mobile', {
+			templateUrl : '/public/app/views/pages/portfolio/buildmate-mobile.html',
+			controller : 'PortfolioController',
+			controllerAs : 'portfolio'
 		});
 
 		// get rid of the hash in url
