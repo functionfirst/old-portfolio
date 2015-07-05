e5ada6a1da9fc1b5967945abcfd063395a7a6262
Alan Jenkins <alan@functionfirst.co.uk>
2015-07-05 13:34:33 +0100

    Highlighting main active nav page and hiding filters when portfolio is not selected.

--- public/app/controllers/mainCtrl.js
+++ public/app/controllers/mainCtrl.js
@@ -2,8 +2,13 @@ angular.module('mainCtrl', ['portfolioService', 'cacheService'])
 
 .controller('MainController', function(Portfolio, CacheService){
 	var main = this;
+	main.page = 'portfolio';
 
 	// FILTERING
+	main.showFilter = function() {
+		return main.page === 'portfolio';
+	}
+
 	CacheService.put('filter', 'all');
 	main.filter = CacheService.get('filter');
 	main.filters = ['axure', 'css', 'c#', 'html', 'javascript', 'node.js', '.net', 'photoshop', 'socket.io']
@@ -12,5 +17,13 @@ angular.module('mainCtrl', ['portfolioService', 'cacheService'])
 		main.filter = filter;
 
 		CacheService.put('filter', filter);
+	}
+
+	main.currentNav = function(page) {
+		main.page = page;
+	}
+
+	main.currentPage = function(page) {
+		return main.page === page;
 	}
 });
