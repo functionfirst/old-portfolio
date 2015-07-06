7605b7357dc83bb857ea0bcc3bad088aef8aa22b
Alan Jenkins <alan@functionfirst.co.uk>
2015-07-06 19:05:31 +0100

    Removed skill filter list as it was somewhat redundant.

--- public/app/controllers/mainCtrl.js
+++ public/app/controllers/mainCtrl.js
@@ -6,45 +6,10 @@ angular.module('mainCtrl', ['portfolioService'])
 	// NAVIGATION
 	main.pages = pages;
 
-	// toggle filter menu
-	main.toggleNav = function(){
-		main.showNav = !main.showNav;
-	};
-
-	main.hideNav = function(){
-		main.showNav = false;
-	}
-
-    // show filters if on the root, portfolio path
-	$scope.$on('$routeChangeSuccess', function(){
-		main.showFilters = $location.path() === '/';
-	});
-
 	// highlight current page
 	main.currentPage = function(page) {
 		return  $location.path() === page;	
 	}
-
-	// FILTERING
-	// Initialise default filter keyword
-	$scope.filter = 'all';
-
-	// available filters
-	// this should probably be generated using the data in portfolio.json
-	main.filters = ['axure', 'css', 'c#', 'html', 'javascript', 'node.js', '.net', 'photoshop', 'socket.io']
-
-	main.filter = {
-		get : function() {
-			return $scope.filter;
-		},
-		set : function(filter) {
-			$scope.filter = filter;
-			main.toggleNav();
-		},
-		current : function(filter) {
-			return $scope.filter === filter;
-		}
-	};
 });
 
 var pages = [{
--- public/app/controllers/portfolioCtrl.js
+++ public/app/controllers/portfolioCtrl.js
@@ -1,30 +1,10 @@
 angular.module('portfolioCtrl', ['portfolioService'])
 
-.filter('technologyFilter', function(){
-	return function(input, term) {
-		if(term === 'all') return input;
-		var out = [];
-
-		angular.forEach(input, function(value, key){
-			angular.forEach(value.keywords, function(keyword){
-				if(keyword === term) {
-					out.push(value);
-				}
-			})
-		});
-		return out;
-	}
-})
-
 .controller('PortfolioController', function($scope, Portfolio){
 	var portfolio = this;
 	portfolio.loading = true;
 
     $scope.pageClass = 'page-portfolio';
-
-	portfolio.filter = function(){
-		return $scope.filter;
-	}
 
 	Portfolio.all().success(function(data){
 		portfolio.loading = false;
