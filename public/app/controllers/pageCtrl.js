6d5bd12735feda6ff1c4d2680df19339eada9cf6
Alan Jenkins <alan@functionfirst.co.uk>
2017-03-01 12:38:54 +0000

    Added content for Claromentis portfolio page and also implemented linking to anchors within the same page.

--- public/app/controllers/pageCtrl.js
+++ public/app/controllers/pageCtrl.js
@@ -2,18 +2,32 @@
     angular.module('pageCtrl', [])
         .controller('PageController', page);
 
-    page.$inject = ['$routeParams'];
+    page.$inject = ['$scope', '$routeParams', '$anchorScroll', '$location'];
 
 
     //////
 
 
-    function page($routeParams){
+    function page($scope, $routeParams, $anchorScroll, $location){
         var vm = this;
         vm.template = $routeParams.path;
+        vm.scrollTo = scrollTo;
 
         if(vm.template) {
             vm.template = ('public/app/views/pages/{0}.html').replace('{0}',vm.template);
         }
+
+        function scrollTo(id) {
+            $location.hash(id);
+            $anchorScroll();
+        } 
+
+
+//         app.controller('TestCtrl', function($scope, $location, $anchorScroll) {
+//    $scope.scrollTo = function(id) {
+//       $location.hash(id);
+//       $anchorScroll();
+//    }
+// });
     }
 })();
